58a8b082858b920f8cd1698a6f5f83a31563d4ea
Jay Honnold <jayhonnold@gmail.com>
2023-01-28 12:17:59 -0700

    Bench: 4271540

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -439,7 +439,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
     
     // if the time is over, we fail hard to stop the search. We don't want to call the system clock
     // too often for speed reasons, so we only apply this when the depth is larger than 6.
-    if ((depth > 6 && !timeManager->isTimeLeft(&td->searchData))) {
+    if (td->nodes % 1024 == 0 && !timeManager->isTimeLeft(&td->searchData)) {
         td->dropOut = true;
         return beta;
     }
